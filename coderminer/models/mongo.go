09406d0d1839f9babf1c2c2e172a673c35a5c526
kevin <kevin@puhanda.com>
2018-06-29 20:14:02 +0800

    add mongodb utils

--- coderminer/models/mongo.go
+++ coderminer/models/mongo.go
@@ -18,7 +18,96 @@ const (
 	direct    = true
 )
 
+var (
+	globalMS *mgo.Session
+)
+
 var s *mgo.Session
+
+func init() {
+	dialInfo := &mgo.DialInfo{
+		Addrs:     []string{dbhost},
+		Direct:    direct,
+		Timeout:   timeout,
+		Source:    authdb,
+		Username:  authuser,
+		Password:  authpass,
+		PoolLimit: poollimit,
+		Mechanism: mechanism,
+	}
+	s, err := mgo.DialWithInfo(dialInfo)
+	if err != nil {
+		log.Fatalf("Create Session: %s\n", err)
+	}
+	globalMS = s
+}
+
+func connect(database, collection string) (*mgo.Session, *mgo.Collection) {
+	ms := globalMS.Copy()
+	c := ms.DB(database).C(collection)
+	return ms, c
+}
+
+func IsEmpty(db, collection string) bool {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+	count, err := c.Count()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return count == 0
+}
+
+func Insert(db, collection string, docs interface{}) error {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+	return c.Insert(docs)
+}
+
+func FindOne(db, collection string, selector, result interface{}) error {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+	return c.Find(selector).One(result)
+}
+
+func FindAll(db, collection string, selector, result interface{}) error {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+	return c.Find(selector).All(result)
+}
+
+func FindIter(db, collection string, selector interface{}) (*mgo.Iter, *mgo.Session) {
+	ms, c := connect(db, collection)
+	return c.Find(selector).Iter(), ms
+}
+
+func Count(db, collection string, query interface{}) (int, error) {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+	return c.Find(query).Count()
+}
+
+func Update(db, collection string, selector, update interface{}) error {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+
+	err := c.Update(selector, update)
+	return err
+}
+
+func Upsert(db, collection string, selector, update interface{}) error {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+
+	_, err := c.Upsert(selector, update)
+	return err
+}
+
+func Remove(db, collection string, selector interface{}) error {
+	ms, c := connect(db, collection)
+	defer ms.Close()
+	return c.Remove(selector)
+}
 
 func GetSession() *mgo.Session {
 	dialInfo := &mgo.DialInfo{
